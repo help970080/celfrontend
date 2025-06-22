4bba244ed1d7117f735bbafacd503cc90ada189b
Leonardo Luna <consultorpremium764@gmail.com>
2025-06-22 15:37:14 -0600

    version 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 // src/main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-// Importación de los estilos
-import './index.css'
-import './thermal-print.css' // <--- LÍNEA AÑADIDA: Importa los estilos para la impresora
+import './index.css';
+// La línea 'import ./thermal-print.css' ha sido eliminada.
 
-import App from './App.jsx'
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
