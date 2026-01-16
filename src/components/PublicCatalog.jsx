b85f765a9b0d9c3becd1150d7c860ea9709e3088
Leonardo Luna <consultorpremium764@gmail.com>
2026-01-16 10:28:44 -0600

    video

--- src/components/PublicCatalog.jsx
+++ src/components/PublicCatalog.jsx
@@ -18,7 +18,7 @@ function PublicCatalog() {
     const [totalItems, setTotalItems] = useState(0);
     const [showFilters, setShowFilters] = useState(false);
 
-    const PROMOTIONAL_VIDEO_URL = "https://youtu.be/fPBLV2kcjKw";
+    const PROMOTIONAL_VIDEO_URL = "https://youtu.be/-_qTxVLIQQI";
 
     const calculateCreditDetails = (price) => {
         const downPaymentPercentage = 0.10;
